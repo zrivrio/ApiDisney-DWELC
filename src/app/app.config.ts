abe7cad59c2abaaa10a40d39fd4e3ef99108a0c7
Zahira <zrivrio2103@gmail.com>
2025-02-25 16:37:05 +0100

    estilo y servicio

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient(withFetch()), provideAnimationsAsync()]
 };
--- src/app/service/disney.service.ts
+++ src/app/service/disney.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DisneyService {
+  private apiUrl = 'https://api.disneyapi.dev';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCharacters() {
+    return this.http.get(`${this.apiUrl}/characters`);
+  }
+
+  getCharacterById(id: string){
+    return this.http.get(`${this.apiUrl}/characters/${id}`);
+  }
+
+  getMovies() {
+    return this.http.get(`${this.apiUrl}/movies`);
+  }
+
+  getMovieById(id: string){
+    return this.http.get(`${this.apiUrl}/movies/${id}`);
+  }
 }
